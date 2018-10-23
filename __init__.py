73f40e6ac00de53b66d2ac0c729b060c1d3c4e2e
Rajesh Rao <rajeshmprao@gmail.com>
2018-10-23 21:17:45 +0530

    added render_template

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 import sys
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return "Hi there, how ya doin?"
+    return render_template('main.html')
 
 if __name__ == "__main__":
     app.run()
